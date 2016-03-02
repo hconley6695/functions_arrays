740bc57beb0ec06f5064e8ca18de984b7579a165
Heather Conley <hconley6695@gmail.com>
2016-03-02 15:56:24 -0500

    answered questions 1 & 2

---
+++ exercise-1.js
@@ -0,0 +1,59 @@
+// Q1
+// Write a function called `sum` that takes two
+// parameters and returns the sum of those 2 numbers.
+
+
+function sum(num_one, num_two) {
+	var add = num_one + num_two;
+	return add;
+}
+
+
+
+// Q2
+// Write a function named `avg` that takes 3 parameters
+// and returns the average of those 3 numbers.
+function avg()
+
+
+
+// Q3
+// Write a function called `getLength` that takes one
+// parameter (a string) and returns the length
+
+function getLength(help) {
+	return help.length;
+}
+
+
+// Q4
+// Write a function called `greaterThan` that takes
+// two parameters and returns `true` if the second
+// parameter is greater than the first. Otherwise
+// the function should return `false`.
+
+
+
+
+// Q5
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+function greet(Name) {
+	var firstname = "Hello, " + Name + "!";
+	return firstname;
+}
+
+
+
+// Q6
+// Write a function called `madlib` that takes
+// 4 or more parameters (words). The function 
+// should insert the words into a pre-defined
+// sentence. Finally the function should return
+// that sentence.
+// Note: When I say words and sentence I mean
+// strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
